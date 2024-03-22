9b7eeacc2fd679760dae50379c88e9f495e7631a
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-03-21 22:05:05 -0700

    hero complete with animiation

--- ecom-snow/src/pages/home/HomePage.jsx
+++ ecom-snow/src/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import {TestComp} from "../../comps/test/TestComp"
 import Navbar from "../../comps/Navbar/Navbar";
+import HomeHero from "../../comps/HomeHero";
 
 export const HomePage = () => {
     return(
@@ -8,6 +9,7 @@ export const HomePage = () => {
         <div>
             {/* <TestComp /> */}
             <Navbar />
+            <HomeHero />
         </div>
         </>
     );
